9509dc07154159094058af33cdbcabcc25947e93
Karim Buksh <161242650+Karim-87@users.noreply.github.com>
2025-05-23 21:27:34 +0500

    assignment 2

---
+++ 04-Assignmets/00_intro_python/Assignment_01_to_05/agreement_bot.py
@@ -0,0 +1,8 @@
+def main():
+    animal: str = input("\033[1;3m What is your favourite animal__? \033[0m ")
+
+    print(f"My favourite animal is {animal}")
+
+
+if __name__ == '__main__':
+    main()
