b7356c79bdb837a169bb73933ba0bc79a8983784
Karim Buksh <161242650+Karim-87@users.noreply.github.com>
2025-05-23 21:03:35 +0500

    Assignment 1

---
+++ 04-Assignmets/00_intro_python/Assignment_01_to_05/add_two_nums.py
@@ -0,0 +1,14 @@
+def main():
+
+    num1: str = input("enter first number:  ")
+    num1: int  = int(num1)
+
+    num2: str = input("enter second number:  ")
+    num2: int = int(num2)
+
+
+    total: int = num1 + num2
+    print(f"This is the sum of two numbers: {total}")
+
+if __name__ == '__main__':
+ main()
